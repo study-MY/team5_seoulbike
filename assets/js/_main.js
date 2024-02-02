2e03c2029c1949f073d79a733c278b817bda400a
LeeMR9 <chesstitans@naver.com>
2024-02-02 09:48:15 +0900

    .

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -33,11 +33,21 @@ function initMap() {
         zoom: 16
     });
 
+    // 공통 스타일 설정
+    var contentStyle1 = 'width: 180px; height: 40px; line-height: 2.7; font-size: 14px; font-weight: 500; text-align: center; background-color: #3D5567; color: #FFF;';
+    var contentStyle2 = 'width: 180px; height: 80px; padding: 10px 29px;';
+    var contentStyle3 = 'width: 150px; font-size: 13px; color: #555555; font-weight: 400;';
+
     // 마커 정보 정의
     var markersInfo = [
-        { position: new naver.maps.LatLng(37.504678975306454, 127.02507115055475), title: "신논현역 4번 출구", content: "정보 내용", number: "23" },
-        { position: new naver.maps.LatLng(37.503283707385826, 127.02476794307636), title: "신논현역 7번 출구", content: "정보 내용", number: "28" },
-        { position: new naver.maps.LatLng(37.50417055795581, 127.0238787848778), title: "신논현역 9번 출구", content: "정보 내용", number: "11" },
+        { position: new naver.maps.LatLng(37.504678975306454, 127.02507115055475), title: "신논현역 4번 출구", content: '<div style="' + contentStyle1 + '">신논현역 4번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 104m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 2</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 8</p></div>', number: "2" },
+        { position: new naver.maps.LatLng(37.503283707385826, 127.02476794307636), title: "신논현역 7번 출구", content: '<div style="' + contentStyle1 + '">신논현역 7번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 129m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 7</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 3</p></div>', number: "7" },
+        { position: new naver.maps.LatLng(37.50417055795581, 127.0238787848778), title: "신논현역 9번 출구", content: '<div style="' + contentStyle1 + '">신논현역 9번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 141m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 1</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 9</p></div>', number: "1" },
+        { position: new naver.maps.LatLng(37.499054924605765, 127.0273894974288), title: "강남역 11번 출구", content: '<div style="' + contentStyle1 + '">강남역 11번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 206m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 4</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 6</p></div>', number: "4" },
+        { position: new naver.maps.LatLng(37.49713083531913, 127.02674092376765), title: "강남역 9번 출구", content: '<div style="' + contentStyle1 + '">강남역 9번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 247m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 3</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 7</p></div>', number: "3" },
+        { position: new naver.maps.LatLng(37.495207504971034, 127.02909080684327), title: "강남역 4번 출구", content: '<div style="' + contentStyle1 + '">강남역 4번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 265m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 8</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 2</p></div>', number: "8" },
+        { position: new naver.maps.LatLng(37.50273163511194, 127.02116161073833), title: "서초4동 주민센터", content: '<div style="' + contentStyle1 + '">서초4동 주민센터</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 407m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 4</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 6</p></div>', number: "4" },
+        { position: new naver.maps.LatLng(37.50461646459502, 127.01396045976747), title: "사평역 1번 출구", content: '<div style="' + contentStyle1 + '">사평역 1번 출구</div><div style="' + contentStyle2 + '"><p style="' + contentStyle3 + '">현재 위치에서 423m</p><p style="' + contentStyle3 + '">대여 가능한 자전거 : 1</p><p style="' + contentStyle3 + '">반납 가능한 거치대 : 9</p></div>', number: "1" },
     ];
 
     var infoWindow;
@@ -49,23 +59,7 @@ function initMap() {
             map: map,
             title: markerInfo.title,
             icon: {
-                content: 
-                '<div style="position: relative; width: 32px; height: 32px; text-align: center;">  <img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;"><span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: $subcolor; font-size: 15px; font-weight: 700;">' + markerInfo.number + '</span></div>',
-                size: new naver.maps.Size(32, 32),
-                anchor: new naver.maps.Point(16, 32)
-            }
-        });
-
-        var marker = new naver.maps.Marker({
-            position: markerInfo.position,
-            map: map,
-            title: markerInfo.title,
-            icon: {
-                content: 
-                '<div class="marker-content">' + 
-                '<img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;">' +
-                '<span>' + markerInfo.number + '</span>' +
-                '</div>',
+                content: '<img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;"><span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: $subcolor; font-size: 15px; font-weight: 700;">' + markerInfo.number + '</span></div>',
                 size: new naver.maps.Size(32, 32),
                 anchor: new naver.maps.Point(16, 32)
             }
@@ -80,7 +74,7 @@ function initMap() {
                 content: markerInfo.content
             });
             infoWindow.open(map, marker);
-        });                  
+        });                      
     });
 
     // 지도 클릭 시 정보 창 닫기
@@ -90,4 +84,3 @@ function initMap() {
         }
     });
 }
-
